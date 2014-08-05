e8d763f378afac492a6df8383f764459a4204b47
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-05 14:58:25 +0300

    generate crud for roles

---
+++ src/DD/ShopBundle/Form/RoleType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DD\ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RoleType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DD\ShopBundle\Entity\Role'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'dd_shopbundle_role';
+    }
+}

dfbe8114096f0148fe911f10c02238f0d112c362
dima7em <semynovich.dmitriy@gmail.com>
2014-08-05 14:26:30 +0300

    add Product

---
+++ src/DD/ShopBundle/Entity/Product.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace DD\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ */
+class Product
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $src;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var \DateTime
+     */
+    private $lastdate;
+
+    /**
+     * @var boolean
+     */
+    private $flag;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set src
+     *
+     * @param string $src
+     * @return Product
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+
+        return $this;
+    }
+
+    /**
+     * Get src
+     *
+     * @return string 
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Product
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set lastdate
+     *
+     * @param \DateTime $lastdate
+     * @return Product
+     */
+    public function setLastdate($lastdate)
+    {
+        $this->lastdate = $lastdate;
+
+        return $this;
+    }
+
+    /**
+     * Get lastdate
+     *
+     * @return \DateTime 
+     */
+    public function getLastdate()
+    {
+        return $this->lastdate;
+    }
+
+    /**
+     * Set flag
+     *
+     * @param boolean $flag
+     * @return Product
+     */
+    public function setFlag($flag)
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    /**
+     * Get flag
+     *
+     * @return boolean 
+     */
+    public function getFlag()
+    {
+        return $this->flag;
+    }
+}

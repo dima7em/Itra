cc900382122ae5c256bf5fdf1c26623e5d42b2ae
dima7em <semynovich.dmitriy@gmail.com>
2014-08-05 14:16:05 +0300

    add Role

---
+++ src/DD/ShopBundle/Entity/Role.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace DD\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Role
+ */
+class Role
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $вуы�description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set вуы�description
+     *
+     * @param string $вуы�description
+     * @return Role
+     */
+    public function setвуы�description($вуы�description)
+    {
+        $this->вуы�description = $вуы�description;
+
+        return $this;
+    }
+
+    /**
+     * Get вуы�description
+     *
+     * @return string 
+     */
+    public function getвуы�description()
+    {
+        return $this->вуы�description;
+    }
+}

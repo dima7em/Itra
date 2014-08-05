79ce4b116e4a57f22aefb05e90eb8ec5c5c284a8
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-05 15:00:28 +0300

    generate crud for categorys

---
+++ src/DD/ShopBundle/Form/CategoryType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DD\ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CategoryType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('flag')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DD\ShopBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'dd_shopbundle_category';
+    }
+}

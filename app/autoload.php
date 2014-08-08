a7eddfddcd3b4b23e4a6dd2c84968de30c34a714
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-08 12:45:32 +0300

    menu

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,11 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+
+
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+
 
 return $loader;
---
+++ src/DD/ShopBundle/Menu/Builder.php
@@ -0,0 +1,20 @@
+<?php
+// src/Acme/DemoBundle/Menu/Builder.php
+namespace DD\ShopBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'dd_shop_homepage'));
+
+        // ... add more children
+
+        return $menu;
+    }
+}

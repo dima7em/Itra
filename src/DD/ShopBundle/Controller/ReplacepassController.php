8ae7a796910bed42e166a7db176d9d08789df501
dima7em <semynovich.dmitriy@gmail.com>
2014-08-14 22:23:40 +0300

    Replasepass with him routs

--- src/DD/ShopBundle/Controller/ReplacepassController.php
+++ src/DD/ShopBundle/Controller/ReplacepassController.php
@@ -5,20 +5,11 @@ use DD\ShopBundle\Entity\User;
 use Symfony\Component\Form\Extension\Validator\Constraints\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\Null;
 
 class ReplacepassController extends Controller
 {
 
-    private function createMyForm(){
-        $password = array('message'=>'Type your message here');
-        $form = $this->createFormBuilder($password)
-            ->add('username', 'text')
-            ->add('email','email')
-            ->add('save', 'submit', array('label' => 'Create Post'))
-            ->getForm();
-
-        return $form;
-    }
     public function replaceAction(Request $request)
     {
         $form=$this->createMyForm();
@@ -30,33 +21,60 @@ class ReplacepassController extends Controller
                 $username = $username['username'];
                 $email = $form->getData();
                 $email = $email['email'];
-                $repository = $this->getDoctrine()->getRepository('DDShopBundle:User');
-                if($user = $repository->findOneBy(array('username'=>$username, 'email'=>$email)))
-                {
-                    $id = $user->getId();
-                    $passkey = sha1(uniqid($email, true));
-                    $user->setPasskey($passkey);
-                    $this->getDoctrine()->getManager()->flush();
-                    $url = $this->container->get('router')->getContext()->getHost().
-                        $this->generateUrl('new_pass', array('id'=>$id, 'passkey'=>$passkey));
-                    $this->sendEmail($id, $email, $url);
-                }
-                else echo('folse');
+                return $this->redirect($this->generateUrl('change',
+                            array('username' => $username, 'email'=>$email)));
             }
         }
-        return $this->render('DDShopBundle:Replacepass:replace.html.twig', array('form' => $form->createView()));
+        return $this->render('DDShopBundle:Replacepass:replace.html.twig',
+                            array('form' => $form->createView()));
     }
 
-    private  function sendEmail($id, $email, $url)
+    private function createMyForm(){
+        $password = array('message'=>'Type your message here');
+        $form = $this->createFormBuilder($password)
+            ->add('username', 'text')
+            ->add('email','email')
+            ->add('save', 'submit', array('label' => 'Create Post'))
+            ->getForm();
+        return $form;
+    }
+
+    public function changeAction($username, $email){
+        $repository = $this->getDoctrine()->getRepository('DDShopBundle:User');
+        if($user = $repository->findOneBy(array('username'=>$username, 'email'=>$email)))
+        {
+            $id = $user->getId();
+            $passkey = sha1(uniqid($email, true));
+            $user->setPasskey($passkey);
+            $this->getDoctrine()->getManager()->flush();
+
+            $url = $this->container->get('router')->getContext()->getHost().
+                    $this->generateUrl('new', array('id'=>$id, 'passkey'=>$passkey));
+
+            return $this->sendEmail($email, $url);
+        }
+        else {
+             $this->get('session')->getFlashBag()->add('notice', 'Password was as');
+             $form=$this->createMyForm();
+            return $this->render('DDShopBundle:Replacepass:replace.html.twig',
+                array('form' => $form->createView()));
+        }
+    }
+
+
+    private  function sendEmail($email, $url)
     {
         $message = \Swift_Message::newInstance()
-            ->setSubject('Hello Email1')
+            ->setSubject('DDShopBundle. Change Pass')
             ->setFrom('semynovich.dmitriy@gmail.com')
             ->setTo($email)
             ->setBody($this->renderView('DDShopBundle:Replacepass:email.txt.twig', array('url'=>$url)), 'text/html');
+
         $this->get('mailer')->send($message);
-    return $this->render('DDShopBundle::layout.html.twig');
+        return $this->redirect($this->generateUrl('message'));
     }
 
-
+    public function messageAction(){
+        return $this->render('DDShopBundle:Replacepass:message.html.twig');
+    }
 }

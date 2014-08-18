23b1e60928ae1899be00e9e82ef34b8003f9b2e0
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-18 19:20:45 +0300

    show

---
+++ src/DD/ShopBundle/Controller/ShowController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DD\ShopBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class ShowController extends Controller
+{
+    public function ShowAction($id)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository('DDShopBundle:Category')
+            ->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category found for id '.$id
+            );
+        }else{
+            $products = $category->getProducts();
+        }
+        return $this->render('DDShopBundle:Catalog:show.html.twig', array('products'=>$products));
+    }
+
+
+}

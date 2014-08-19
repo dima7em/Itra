70307b6c00e2cec8cc48d9e43c6b0649bde226cf
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-19 19:22:02 +0300

    showw

--- src/DD/ShopBundle/Controller/CatalogController.php
+++ src/DD/ShopBundle/Controller/CatalogController.php
@@ -8,6 +8,29 @@ class CatalogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DDShopBundle:Catalog:main.html.twig');
+        $request = $this->get('request');
+        $id = $request->query->get('id');
+        if (!$id){
+            return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
+        }
+        $category = $this->getDoctrine()
+            ->getRepository('DDShopBundle:Category')
+            ->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category found for id '.$id
+            );
+        }else{
+
+            $products = $category->getProducts();
+            if (!$products) {
+                return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
+            }
+
+            return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>$products));
+
+        }
+
     }
 }

40ee29bef80453d4662175dab06fe99d5b4a3bd0
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-21 00:17:57 +0300

    first part of sort

--- src/DD/ShopBundle/Controller/CatalogController.php
+++ src/DD/ShopBundle/Controller/CatalogController.php
@@ -8,13 +8,17 @@ class CatalogController extends Controller
 {
     public function indexAction()
     {
+        /*get param from url*/
         $request = $this->get('request');
         $id = $request->query->get('id');
         $sort = $request->query->get('sort');
         $direction = $request->query->get('direction');
+
         if (!$id){
             return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
         }
+
+        /*get category */
         $category = $this->getDoctrine()
             ->getRepository('DDShopBundle:Category')
             ->find($id);
@@ -25,16 +29,31 @@ class CatalogController extends Controller
             );
         }else{
 
+            /*Get products*/
+
             $products = $category->getProducts();
             if (!$products) {
                 return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
             }
+
+            /*make pagination for products*/
+
             $paginator  = $this->get('knp_paginator');
             $pagination = $paginator->paginate(
                 $products,
                 $this->get('request')->query->get('page', 1)/*page number*/,
                 5/*limit per page*/
             );
+
+
+            /*sort products */
+            if($sort){
+                $index = array();
+                foreach($pagination as $a) $index[] = $a->$sort;
+                array_multisort($index, $pagination, $direction);
+
+            }
+
 
             return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>$pagination));
 

f7543d21d4c50c8e2e9872d1fbea3fc4a463053c
dnazarevich <nazarevichdima1993@gmail.com>
2014-08-20 23:03:48 +0300

    add paginator for products in catalog

--- src/DD/ShopBundle/Controller/CatalogController.php
+++ src/DD/ShopBundle/Controller/CatalogController.php
@@ -10,6 +10,8 @@ class CatalogController extends Controller
     {
         $request = $this->get('request');
         $id = $request->query->get('id');
+        $sort = $request->query->get('sort');
+        $direction = $request->query->get('direction');
         if (!$id){
             return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
         }
@@ -27,8 +29,14 @@ class CatalogController extends Controller
             if (!$products) {
                 return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>''));
             }
+            $paginator  = $this->get('knp_paginator');
+            $pagination = $paginator->paginate(
+                $products,
+                $this->get('request')->query->get('page', 1)/*page number*/,
+                5/*limit per page*/
+            );
 
-            return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>$products));
+            return $this->render('DDShopBundle:Catalog:main.html.twig', array('products'=>$pagination));
 
         }
 

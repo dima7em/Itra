5636531a4a3f541853af9027e56c452622c9ed78
dima7em <semynovich.dmitriy@gmail.com>
2014-08-20 12:40:20 +0300

    merge security

--- src/DD/ShopBundle/Controller/ImgController.php
+++ src/DD/ShopBundle/Controller/ImgController.php
@@ -1,6 +1,7 @@
 <?php
 namespace DD\ShopBundle\Controller;
 
+use Cloudinary\Api;
 use Cloudinary\Uploader;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,14 +9,12 @@ class ImgController extends Controller
 {
     public function imgAction()
     {
-        $cloudinary = new \Cloudinary();
-        $cloudinary::config(array(
-            "cloud_name"=>"localhost-all-web",
-            "api_key"=>"787221372966778",
-            "api_secret"=>"X3fO_ct2jQUBucxkBn7XyhQ85hM"
-        ));
+
+       //Cloudinary:Uploader::destroy('xjlkykhm9yjxdttb8dmn');
+
         //:Uploader::upload("http://upload.wikimedia.org/wikipedia/commons/a/af/Bonsai_IMG_6426.jpg");
-        Cloudinary:Uploader::upload("http://upload.wikimedia.org/wikipedia/commons/a/af/Bonsai_IMG_6426.jpg");
-        echo cl_image_tag("beurlw94buorqn7v2vs3.jpg", array( "alt" => "Sample Image" ));
-    }
+       // Cloudinary:Uploader::upload("http://upload.wikimedia.org/wikipedia/commons/a/af/Bonsai_IMG_6426.jpg");
+        echo cl_image_tag("mdrn2mwsjntzodfugejp.jpg", array( "alt" => "Sample Image" ));
+
+       }
 }
